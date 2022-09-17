50d8ce128add08d2aaf86de8e31030b9e9f336c2
Weronika2022 <weronikarfk@gmail.com>
2022-09-17 13:08:28 +0200

    dodanie komentarzy

--- cypress/e2e/testy_werki h&m.cy.js
+++ cypress/e2e/testy_werki h&m.cy.js
@@ -7,31 +7,37 @@ Cypress.on('uncaught:exception', (err, runnable) => {
 })
 
 describe('moje pierwsze testy automatyczne', () => {
+    // przejscie do strony h&m
     beforeEach(() => {
       cy.visit('https://www2.hm.com/pl_pl/index.html')
       cy.viewport(1900, 750)
     })
+    // akceptacja coockiesow
     it('Accept cookies', () => {
         // cy.xpath('//*[@id="onetrust-accept-btn-handler"]').click()
         // cy.wait(1000)
         cy.xpath('//*[@id="onetrust-accept-btn-handler"]').contains('Akceptuj').click({force: true})
 
       })
+    // kilkamy link ona  
     it('Kliknięcie pierwszego linku-Ona', () => {
       cy.xpath('//*[@id="onetrust-accept-btn-handler"]').contains('Akceptuj').click({force: true})
       cy.wait(3000)
       cy.xpath('/html/body/header/nav/ul[2]/li[2]/a/span').click()
     })
+    // klikam szósty link
     it('sixth link check', () => {
       cy.xpath('//*[@id="onetrust-accept-btn-handler"]').contains('Akceptuj').click({force: true})
 
       cy.xpath('/html/body/div/div/div/div/center/div/div[7]/a/div/div/h6').click()
     })
+    // klikam ósmy link
     it('eighth link check', () => {
       cy.xpath('//*[@id="onetrust-accept-btn-handler"]').contains('Akceptuj').click({force: true})
 
       cy.xpath('/html/body/div/div/div/div/center/div/div[9]/a/div/div/h6').click()
     })
+    // klikam siódmy link
     it('seventh link check', () => {
       cy.xpath('//*[@id="onetrust-accept-btn-handler"]').contains('Akceptuj').click({force: true})
 
